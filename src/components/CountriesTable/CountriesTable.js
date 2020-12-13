db417f868a000759b395ad0eee3ef542aea08d0f
Kato <kubkate@gmail.com>
2020-12-13 09:50:36 -0800

    add sorting by population

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,6 +1,21 @@
 import styles from "./Countries.module.css";
 
+const orderBy = (countries, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) => (a.population > b.population ? 1 : -1));
+  }
+
+  if (direction === "desc") {
+    return [...countries].sort((a, b) => (a.population > b.population ? -1 : 1));
+  }
+
+  // by default
+  return countries;
+}
+
 const CountriesTable = ({countries}) => {
+  const orderedCountries = orderBy(countries, "desc");
+
   return (
     <div>
         <div className={styles.heading}>
@@ -12,7 +27,7 @@ const CountriesTable = ({countries}) => {
         </button>
       </div>
 
-        {countries.map((country,idx) => (
+        {orderedCountries.map((country,idx) => (
           <div key={country+idx} className={styles.row}>
             <div className={styles.name}>{country.name}</div>
             <div className={styles.population}>{country.population}</div>
