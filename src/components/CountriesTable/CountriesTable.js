6c98c03777793f3660cb9a5ed2b69809da3685fa
Kato <kubkate@gmail.com>
2020-12-13 10:44:27 -0800

    update logic to dynamicly use of value and direction

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -2,13 +2,13 @@ import { KeyboardArrowDownRounded, KeyboardArrowUpRounded } from "@material-ui/i
 import { useState } from "react";
 import styles from "./Countries.module.css";
 
-const orderBy = (countries, direction) => {
+const orderBy = (countries, value, direction) => {
   if (direction === "asc") {
-    return [...countries].sort((a, b) => (a.population > b.population ? 1 : -1));
+    return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
   }
 
   if (direction === "desc") {
-    return [...countries].sort((a, b) => (a.population > b.population ? -1 : 1));
+    return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
   }
 
   // by default
@@ -40,7 +40,7 @@ const CountriesTable = ({countries}) => {
   const [direction, setDirection] = useState();
   const [value, setValue] = useState();
 
-  const orderedCountries = orderBy(countries, "desc");
+  const orderedCountries = orderBy(countries, value, direction);
 
   const switchDirection = () => {
     if (!direction) {
@@ -52,18 +52,25 @@ const CountriesTable = ({countries}) => {
     }
   }
 
+  const setValueAndDirection = (value) => {
+    switchDirection();
+    setValue(value)
+  }
 
   return (
     <div>
         <div className={styles.heading}>
-        <button className={styles.heading_name}>
+        <button 
+          className={styles.heading_name}
+          onClick={() => setValueAndDirection("name")}
+        >
           <div>Name</div>
-          <SortArrow />
+          <SortArrow direction={direction} />
           
         </button>
         <button 
           className={styles.heading_population}
-          onClick={switchDirection}
+          onClick={() => setValueAndDirection("population")}
         >
           <div>Population</div>
           <SortArrow direction={direction} />
