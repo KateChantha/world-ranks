2c24bf3677bf9b66ec84fa1ed89168202431a8f4
Kato <kubkate@gmail.com>
2020-12-12 18:51:12 -0800

    add icon and classname

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,9 +1,13 @@
-const SreachInput = () => {
+import SearchRounded from "@material-ui/icons/SearchRounded";
+import styles from "./SearchInput.module.css;"
+
+const SearchInput = ({ ...rest }) => {
   return (
-    <div>
-      <input />
+    <div className={styles.wrapper}>
+      <SearchRounded />
+      <input className={styles.input} {...rest}/>
     </div>
   )
 }
 
-export default SreachInput;
+export default SearchInput;
