2d3b157f27df77c662774c8798caf00bce19657c
Kato <kubkate@gmail.com>
2020-12-12 17:37:27 -0800

    initail - wire up Home to Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,9 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/layout/Layout';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        This is a main.
-      </main>
-
-      <footer className={styles.footer}>
-        This is a footer.
-      </footer>
-    </div>
-  )
+   <Layout>main</Layout>
+  );
 }
