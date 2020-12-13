1b722bddf52c2f4177f70790d6593093ee8ba151
Kato <kubkate@gmail.com>
2020-12-12 18:21:59 -0800

    fetch data

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,24 @@ import Head from 'next/head'
 import Layout from '../components/layout/Layout';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log("IN Home", countries);
   return (
-   <Layout>main</Layout>
+   <Layout>
+    <div className={styles.counts}>Found {countries.length} countries</div>
+   </Layout>
   );
 }
+
+/*https://nextjs.org/docs/basic-features/data-fetching */
+// get all the data at the build time
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
