2ac08f89f7eb5c648412bbaa472f4ad834fd5c2c
Kato <kubkate@gmail.com>
2020-12-14 16:28:22 -0800

    fetch country for each country page

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,7 +1,20 @@
-const Country = () => {
+const Country = ({ country }) => {
+  console.log("in [id]- Country", country)
+
   return (
     <div>Country</div>
   )
 }
 
 export default Country;
+/** whenever we visit the page, fetch data in the server before we render it */
+export const getServerSideProps = async ({ params }) => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${params.id}`);
+  const country = await res.json();
+
+  return {
+    props: {
+      country,
+    }
+  }
+}
