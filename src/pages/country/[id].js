aa4873592392ab2405cb9da1a13d8837260197a7
Kato <kubkate@gmail.com>
2020-12-14 17:27:58 -0800

    add details and style

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,8 +1,31 @@
+import Layout from "../../components/layout/Layout";
+import styles from "./country.module.css"
+
 const Country = ({ country }) => {
   console.log("in [id]- Country", country)
 
   return (
-    <div>Country</div>
+    <Layout title={country.name}>
+      <div>
+        <div className={styles.overview_panel}>
+          <img src={country.flag} alt={country.name}/>
+          <h1 className={styles.overview_name}>{country.name}</h1>
+          <div className={styles.overview_region}>{country.region}</div>
+
+          <div className={styles.overview_data}>
+            <div className={styles.overview_population}>
+              <div className={styles.overview_value}>{country.population}</div>
+              <div className={styles.overview_label}>Population</div>
+            </div>
+            <div className={styles.overview_area}>
+              <div className={styles.overview_value}>{country.area}</div>
+              <div className={styles.overview_label}>Area</div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </Layout>
   )
 }
 
